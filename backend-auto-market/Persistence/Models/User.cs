c00f2c2dff86a1934eb7ba8370bf628d51293ff8
Hlib Zoma <hlibzoma@gmail.com>
2025-10-23 19:56:16 +0200

    add user entity

---
+++ backend-auto-market/Persistence/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace backend_auto_market.Persistence.Models;
+
+public class User : BaseEntity
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Country { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+
+    public string? About { get; set; }
+    public string? AvatarUrl { get; set; }
+    public string? Address { get; set; }
+
+    public bool IsGoogleAuth { get; set; } = false;
+}
---
+++ backend-auto-market/Services/Token.cs
@@ -0,0 +1,7 @@
+﻿namespace backend_auto_market.Services;
+
+public class Token
+{
+    public string TokenKey { get; set; } = string.Empty;
+    public DateTime Expires { get; set; }
+}
