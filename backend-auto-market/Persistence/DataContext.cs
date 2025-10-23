0b76525c19736c8295ebc35df471a4599f0bb6eb
Hlib Zoma <hlibzoma@gmail.com>
2025-10-23 21:21:10 +0200

    remove duplicate code

--- backend-auto-market/Persistence/DataContext.cs
+++ backend-auto-market/Persistence/DataContext.cs
@@ -31,6 +31,4 @@ public class DataContext(DbContextOptions options) : DbContext(options)
 
         return await base.SaveChangesAsync(cancellationToken);
     }
-
-    public DbSet<User> Users { get; set; }
 }
