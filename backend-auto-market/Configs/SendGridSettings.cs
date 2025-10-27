6fbae346dea5cbd9f82b05742237a2cd16463bfa
Hlib Zoma <hlibzoma@gmail.com>
2025-10-27 18:58:22 +0100

    Revert "SendGrid test"
    This reverts commit e7be487da80abea72661a1cccac504096740d06a.

--- backend-auto-market/Configs/SendGridSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace backend_auto_market.Configs;
-
-public class SendGridSettings
-{
-    public string ApiKey { get; set; } = null!;
-    public string FromEmail { get; set; } = null!;
-    public string FromName { get; set; } = "AutoMarket";
-}
