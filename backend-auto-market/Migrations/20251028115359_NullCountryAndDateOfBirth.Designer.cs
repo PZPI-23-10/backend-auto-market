8da42b68deef128ac6e5b5860626ebadec3986be
Hlib Zoma <hlibzoma@gmail.com>
2025-10-28 12:54:13 +0100

    Make Country and DateOfBirth nullable in User model

--- backend-auto-market/Migrations/20251028115359_NullCountryAndDateOfBirth.Designer.cs
+++ backend-auto-market/Migrations/20251028115359_NullCountryAndDateOfBirth.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using backend_auto_market.Persistence;
@@ -11,9 +12,11 @@ using backend_auto_market.Persistence;
 namespace backend_auto_market.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20251028115359_NullCountryAndDateOfBirth")]
+    partial class NullCountryAndDateOfBirth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
