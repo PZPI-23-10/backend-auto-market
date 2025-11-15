4b40cabb76467990012717d1873930542cbebc60
Hlib Zoma <hlibzoma@gmail.com>
2025-11-15 11:41:09 +0100

    fix: make last name optional

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -5,7 +5,7 @@ public class User : BaseAuditableEntity
     public string Email { get; set; }
     public string? Password { get; set; }
     public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string? LastName { get; set; }
     public string? PhoneNumber { get; set; }
     public string? Country { get; set; }
     public string? AboutYourself { get; set; }
